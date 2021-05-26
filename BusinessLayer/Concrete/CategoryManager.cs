7a143d8d1904723552591f79623e7d1116f75a42
omerserfice <srfcomr@gmail.com>
2021-05-26 17:23:28 +0300

    Add project files.

---
+++ BusinessLayer/Concrete/CategoryManager.cs
@@ -0,0 +1,57 @@
+﻿using EntityLayerr.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccessLayer.Concrete.Repositories;
+using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+
+namespace BusinessLayer.Concrete
+{
+    public class CategoryManager : ICategoryService
+    {
+        ICategoryDal _categoryDal;
+
+        public CategoryManager(ICategoryDal categoryDal)
+        {
+            _categoryDal = categoryDal;
+        }
+
+        public void CategoryAdd(Category category)
+       {
+            _categoryDal.Insert(category);
+        }
+
+        public void CategoryAddBL(Category category)
+        {
+
+            _categoryDal.Insert(category);
+
+          }
+
+        public void CategoryDelete(Category category)
+        {
+            _categoryDal.Delete(category);
+        }
+
+        public void CategoryUpdate(Category category)
+        {
+            _categoryDal.Update(category);
+        }
+
+        public Category GetByID(int id)
+        {
+            return _categoryDal.Get(x => x.CategoryID == id);
+        }
+
+        public List<Category> GetList()
+        {
+            return _categoryDal.List();
+        }
+    }
+
+
+
+}
---
+++ MvcProje/Controllers/StatisticController.cs
@@ -0,0 +1,36 @@
+﻿using DataAccessLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcProje.Controllers
+{
+    public class StatisticController : Controller
+    {
+        Context context = new Context();
+        // GET: Statistic
+        public ActionResult Index()
+        {
+            var deger = context.Categorys.Count().ToString();
+            ViewBag.viewbag = deger;
+
+            var deger1 = context.Headings.Count(x=>x.CategoryID==8).ToString();
+            ViewBag.viewbag1 = deger1;
+
+            var deger2 = context.Writers.Count(x => x.WriterName.Contains("a"));
+            ViewBag.viewbag2 = deger2;
+
+            var deger3 = context.Categorys.Where(x=>x.CategoryID==context.Headings.GroupBy(h => h.CategoryID)
+            .OrderByDescending(h => h.Count()).Select(h => h.Key).FirstOrDefault()).
+            Select(h => h.CategoryName).FirstOrDefault();
+            ViewBag.viewbag3 = deger3;
+
+            var deger4 = Math.Abs(context.Categorys.Count(x => x.CategoryStatus == true) - context.Categorys.Count(x => x.CategoryStatus == false));
+            ViewBag.viewbag4 = deger4;
+            return View();
+
+        }
+    }
+}
